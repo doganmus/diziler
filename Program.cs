b65ddc3976f7db1eab4e1ba9705342028c5f9b0a
Dogan Mus <doganmus87@gmail.com>
2022-04-03 23:51:58 +0300

    diziler öğrenim tamam

--- Program.cs
+++ Program.cs
@@ -6,7 +6,42 @@ namespace diziler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //dizi tanımlama
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = {"kedi","köpek","kuş","maymun"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //dizilere değer atama ve erişim
+            renkler[0]= "Mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //dögüler ile dizi kullanımı
+            //klavyeden girilen n tane sayının ortalamasını hesaplayan program
+
+            Console.Write("Lütfen dizinin eleman sayısını giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi =new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı giriniz: ",i+1);
+                sayiDizisi[i] =int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+               toplam +=sayi;
+            }
+            Console.WriteLine("Ortalama: " + toplam/diziUzunlugu);
+
         }
     }
 }
